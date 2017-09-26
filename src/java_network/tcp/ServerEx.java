66e970dfed80c7cfdd9e006ebf2ef4b3d3c2cdb4
Mosquito(A) <skykim10908@naver.com>
2017-09-26 16:11:27 +0900

    채팅 예제

--- src/java_network/tcp/ServerEx.java
+++ src/java_network/tcp/ServerEx.java
@@ -14,42 +14,40 @@ public class ServerEx {
 		BufferedReader in = null; // 클라이언트로 부터 보낸 메시지를 읽기위한 Reader
 		BufferedReader stIn = null; // 키보드로 부터 입력받기 위한 Reader
 		BufferedWriter out = null; // 클라이언트로 보낼 Writer
-		
+
 		ServerSocket listener = null; // 서버 소켓(클라이언트의 요청을 대기하는 소켓)
 		Socket socket = null; // 클라이언트와 주고받을 소켓
-		
+
 		try {
-			listener = new ServerSocket(9999); // 
+			listener = new ServerSocket(9999); //
 			System.out.println("Server Ready~~!!");
 			socket = listener.accept();
 			System.out.println("클라이언트와  연결되었습니다.");
-			
+
 			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			stIn = new BufferedReader(new InputStreamReader(System.in));
-			
+
 			out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-		
+
 			String clientMsg;
-			while(true){
+			while (true) {
 				clientMsg = in.readLine();
-				if(clientMsg.equalsIgnoreCase("bye")){
+				if (clientMsg.equalsIgnoreCase("bye")) {
 					break;
-				}System.out.println(clientMsg);
-				 String outputMsg = stIn.readLine();
-				 out.write(outputMsg);
-				 out.flush();
+				}
+				System.out.println(clientMsg);
+				String outputMsg = stIn.readLine();
+				out.write(" 서버 >" + outputMsg);
+				out.flush();
 			}
-		
+
 		} catch (IOException e) {
-			
+
 			e.printStackTrace();
-		}finally{
+		} finally {
 			try {
 				socket.close();
 				listener.close();
-				out.close();
-				in.close();
-				stIn.close();
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
