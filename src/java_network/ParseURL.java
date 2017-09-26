b1f39f23a9fd8f1a87c0b9eb62900e99dbce734a
Mosquito(A) <skykim10908@naver.com>
2017-09-26 15:04:11 +0900

    교재 p794 예제 16-2으로 수정

--- src/java_network/ParseURL.java
+++ src/java_network/ParseURL.java
@@ -1,9 +1,14 @@
 package java_network;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 
 import javax.swing.JOptionPane;
+
+import com.sun.jndi.url.iiopname.iiopnameURLContextFactory;
 
 public class ParseURL {
 
@@ -21,8 +26,17 @@ public class ParseURL {
 			System.out.printf("port : %s%n", opinion.getPort());
 			System.out.printf("path : %s%n", opinion.getPath());
 			System.out.printf("filename : %s", opinion.getFile());
+		
+			BufferedReader br = new BufferedReader(new InputStreamReader(opinion.openStream()));
+			String line;
+			while( (line = br.readLine()) != null){
+				System.out.println(line);
+			}br.close();
+			
 		} catch (MalformedURLException e) {
 			JOptionPane.showMessageDialog(null, "肋给等 URL 林家");
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 
 	}
